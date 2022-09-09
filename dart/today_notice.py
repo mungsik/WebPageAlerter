84fdc14bb0ba2d5a8a2ca043dcce7b08b8f040e0
mungsik <sparkin154@gmail.com>
2022-09-09 21:21:00 +0900

    실시간 mysql로 데이터 전송 성공

--- dart/today_notice.py
+++ dart/today_notice.py
@@ -7,7 +7,6 @@ import os
 import pymysql
 
 from dotenv import load_dotenv
-from calendar import c
 from bs4 import BeautifulSoup
 
 
@@ -19,7 +18,7 @@ from bs4 import BeautifulSoup
 
 url = "https://dart.fss.or.kr/dsac001/mainAll.do"
 baseURL = "https://dart.fss.or.kr"
-
+contents = []
 # 새로운 공시 정보를 받아오는 함수
 def get_new_contents(old_contents=[]):
     response = requests.get(url)
@@ -29,8 +28,6 @@ def get_new_contents(old_contents=[]):
         soup.find("table", attrs={"class": "tbList"}).find("tbody").find_all("tr")
     )
 
-    notices = []
-
     for row in data_rows:
         columns = row.find_all("td")
         # 의미 없는 데이터 skip
@@ -38,22 +35,19 @@ def get_new_contents(old_contents=[]):
         if len(columns) <= 1:
             continue
 
-        contents = [
-            columns[1].get_text()[3:],
-            columns[2].get_text(),
-            columns[4].get_text(),
-            baseURL + columns[2].find("a")["href"],
-        ]
-
-        # 요소에서 컨텐츠만 추출해서 리스트로 저장
-        contents = [w.strip().replace("\t", "").replace("\n", "") for w in contents]
+        contents.append(
+            [
+                columns[1].get_text()[3:].strip().replace("\t", "").replace("\n", ""),
+                columns[2].get_text().strip().replace("\t", "").replace("\n", ""),
+                columns[4].get_text().strip().replace("\t", "").replace("\n", ""),
+                baseURL + columns[2].find("a")["href"],
+            ]
+        )
 
         # 기존의 컨텐츠와 신규 컨텐츠를 비교해서 새로운 컨텐츠만 저장
         new_contents = [content for content in contents if content not in old_contents]
 
-        notices.append(new_contents)
-
-        return notices
+    return new_contents
 
 
 # 새로운 공시 정보 추출
@@ -68,9 +62,36 @@ def real_time_info():
     if new_contents:
         for content in new_contents:
             print("new", content)
+            with open("today_notice.csv", "a") as f:
+                writer = csv.writer(f)
+                writer.writerow(content)
+
     # 없으면 패스
     else:
         pass
+
+    csv_data = pd.read_csv("today_notice.csv")
+    csv_data = csv_data.where((pd.notnull(csv_data)), None)
+
+    load_dotenv()
+
+    db = pymysql.connect(
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        db=os.environ.get("DB_NAME"),
+        charset="utf8",
+    )
+
+    curs = db.cursor()
+
+    sql = "INSERT INTO today_notice(company, report_head, date, url) VALUES (%s, %s, %s, %s)"
+
+    for idx in range(len(csv_data)):
+        curs.execute(sql, tuple(csv_data.values[idx]))
+
+    db.commit()
+    db.close()
 
     # 기존 링크를 계속 축적하기 위함
     old_contents += new_contents.copy()
@@ -84,7 +105,7 @@ if __name__ == "__main__":  # 프로그램의 시작점일 때만 아래 코드 
     old_contents = []
 
     # 주기적 실행과 관련된 코드 (hours는 시, minutes는 분, seconds는 초)
-    job = schedule.every(10).seconds.do(real_time_info)
+    job = schedule.every(30).seconds.do(real_time_info)
 
     while True:
 
